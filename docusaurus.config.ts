06337d12501c0379e638cfa924e1e6022593fab2
jhayesCBH <jhayes@beverlyhills.org>
2024-04-23 13:08:30 -0700

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,83 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type {Options as DocsOptions} from '@docusaurus/plugin-content-docs';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Appointment Request Docs',
+  tagline: 'Appointment Request Docs',
+  favicon: 'https://beverlyhills.org/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'CBHFinance', // Usually your GitHub org/user name.
+  projectName: 'ARFHelpDocs', // Usually your repo name.
+  trailingSlash: true,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: [
+            require.resolve('./src/css/custom.css'),
+            // require.resolve('@infinum/docusaurus-theme/dist/style.css'),
+          ] 
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  markdown: {
+    format: 'detect',
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
+  themeConfig: {
+    image: 'img/cbh_arfDocs_socialCard.png',
+    navbar: {
+      title: 'ARF Docs',
+      logo: {
+        alt: 'CBH Shield',
+        src: 'img/cbh_logo_light.svg',
+        srcDark: 'img/cbh_logo_dark.svg',
+        href: '/',
+      },
+    },
+    footer: {},
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    // mermaid: {
+    //   theme: {light: 'neutral', dark: 'forest'},
+    //   options: {
+    //     maxTextSize: 50,
+    //   },
+    // },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
